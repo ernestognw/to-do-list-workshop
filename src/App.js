53b88ebdbc80db5201d6908be00f9b006c63568e
ernestognw <ernestognw@gmail.com>
2018-11-21 00:48:13 -0600

    Step 1 - Hello world

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1 className="title">Hello world <span aria-label="emoji" role="img">🔥</span></h1>
+        <input type="text" className="new-task"/>
       </div>
     );
   }
