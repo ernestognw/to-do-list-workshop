11467da7c753625e802588fbfe41f1efa82acb71
ernestognw <ernestognw@gmail.com>
2018-11-21 00:57:06 -0600

    Step 2 - Manejo de estado y change handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTask: "",
+    }
+  }
+
+  handleTaskChange = (event) => {
+    this.setState({
+      newTask: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <h1 className="title">Hello world <span aria-label="emoji" role="img">🔥</span></h1>
-        <input type="text" className="new-task"/>
+        <input onChange={this.handleTaskChange} type="text" className="new-task"/>
+        <h2 className="test-label">{this.state.newTask}</h2>
       </div>
     );
   }
